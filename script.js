e6539b7cccf1fc4f7db7f586ff252d0a21729034
ajmalMundumuzhi <149889538+ajmalMundumuzhi@users.noreply.github.com>
2024-07-06 22:43:44 +0530

    new one

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ document.getElementById('menu-toggle').addEventListener('click', function() {
         nav.style.display = 'flex';
     }
 });
+<script>
+    function showSidebar(){
+      const sidebar = document.querySelector('.sidebar')
+      sidebar.style.display = 'flex'
+    }
+    function hideSidebar(){
+      const sidebar = document.querySelector('.sidebar')
+      sidebar.style.display = 'none'
+    }
+  </script>
